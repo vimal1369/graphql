a4ff0ea26588893cc359b11f744f34c0bdd7325f
vimal1369 <vim13al@gmail.com>
2018-05-24 00:25:25 +0530

    added data

---
+++ schema.js
@@ -0,0 +1,17 @@
+import {makeExecutableSchema, addMockFunctionsToSchema} from 'graphql-tools';
+import resolvers from './resolvers.js'
+const typeDefs = `type Author {
+    id: Int
+    age: Int
+    name: String
+    books: [String]
+}
+    type Query {
+        authors: [Author]
+        author(id: Int): Author
+    }
+`; 
+
+const schema = makeExecutableSchema({typeDefs, resolvers});
+
+export default schema;
---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import {graphqlExpress, graphiqlExpress} from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './schema.js';
+const server = express();
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: "/graphql"
+}));
+
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+
+server.listen(4000, () => {
+    console.log('listening on port 4000');
+});
